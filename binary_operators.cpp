459355ed6bebcb6c23f250aab0573669111180a5
Soumili Biswas <soumilibiswas.github@gmail.com>
2026-05-06 12:03:37 +0530

    Added operator and operand C++ exercises

---
+++ binary_operators.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<16.8/6<<endl;
+    cout<<13*4;
+}
---
+++ unary_operators.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a = 10;
+    cout<<a++<<endl; // post increment
+    cout<<a<<endl;
+
+
+
+    int b = 6;
+    cout<<++b<<endl;
+    cout<<b;
+}
