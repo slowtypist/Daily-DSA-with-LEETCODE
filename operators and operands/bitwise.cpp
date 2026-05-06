cd712aae71c7b6f3deaadaf2ad43f375a923ce0d
Soumili Biswas <soumilibiswas.github@gmail.com>
2026-05-06 13:18:00 +0530

    Add files via upload

---
+++ operators and operands/bitwise.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    //& operator
+    int ans = 2&3;
+    cout<<ans<<endl;
+
+    // | operators
+    int ans1 = 2|3;
+    cout<<ans1<<endl;
+
+
+
+    //xor
+    int ans2 = 2^3;
+    cout<<ans2<<endl;
+
+    //left shift
+    int ans8 = 2<<3;
+    cout<<ans8<<endl;
+
+
+
+    //right shift
+    int ansa =22>>3;
+    cout<<ansa<<endl;
+
+
+
+    //compliment 
+    int name  = ~7; //increase 1 and with a negative sign
+    cout<<name<<endl;
+}
+
