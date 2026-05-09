c864fa3dcaf005c67973dd880b84ccdd7e5515e7
Soumili Biswas <soumilibiswas.github@gmail.com>
2026-05-09 09:14:11 +0530

    Update pattern20.cpp

--- loops practice part 1/pattern20.cpp
+++ loops practice part 1/pattern20.cpp
@@ -1,14 +1,13 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 int main()
 {
-    for(int row = 1; row<= 5 ; row++)
-    {
-        for(int col = 5 ; col<=5-(row-1) ; col++)
-        {
-            cout<<col<<" ";
-        }
-        cout<<endl;
-    }
-    
+    for(int row = 5 ; row >=1 ; row--)
+{ 
+for (int col = 5 ; col >= row ; col --)
+{
+cout<<col<<" ";
+}
+cout<<endl;
+}
 }
