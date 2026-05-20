a905f3e9a3f81ac3bcb8db77d4d08299adc50719
Soumili Biswas <soumilibiswas.github@gmail.com>
2026-05-20 13:21:14 +0530

    Add example program using std::vector
    Implement a simple program to demonstrate vector usage.

---
+++ stl vector/syn1.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main()
+{
+    vector<int> vec;
+    vec.push_back (10);
+    vec.push_back (20); 
+    vec.push_back(30);
+    vec.push_back(40);
+    vec.push_back(60);
+    cout<< " the elemnets are :";   // too peint all tthe elemnest of the vector that is an dynamic  array
+    for (int val: vec)
+    {
+        cout<<val<<" ";
+    }
+    return 0;
+}
